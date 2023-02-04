e24fa98ff3915ace6126abf9367925d7a3e014f9
Musicians and Singer <markharvey"proclaimers.com>
2023-02-04 19:12:01 +0000

    switched midi out

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from datetime import datetime, timedelta
 
 import mido as mido
+import pygame
+
+
+STOP_BUTTONS = {5, 15, 23, 35, 45, 55, 65, 75, 85, 95}
+REPEAT_BUTTONS = {6, 16, 26, 36, 46, 66, 66, 76, 86, 96}
+RESUME_BUTTONS = {7, 17, 23, 37, 47, 77, 67, 77, 87, 97}
+
+
+ON = 127
+
+"""
+How to install if required
+https://wiki.python.org/moin/WindowsCompilers#Microsoft_Visual_C.2B-.2B-_14.2_standalone:_Build_Tools_for_Visual_Studio_2019_.28x86.2C_x64.2C_ARM.2C_ARM64.29
+"""
+pygame.init()
+mido.set_backend("mido.backends.pygame")
 
 if __name__ == '__main__':
+
     out_names = mido.get_output_names()
     print(out_names)
 
     # in_port = mido.open_input('HELIX   ')
-    in_port = mido.open_input('USB MIDI Interface')
-    out_port = mido.open_output('PY OUT', virtual=True, client_name='PY OUT')
+    # in_port = mido.open_input('USB MIDI Interface')
+    # out_port = mido.open_output('loopMIDI Port')
 
     print("starting")
 
@@ -29,11 +46,24 @@ if __name__ == '__main__':
     # ...
     # pedal 10 = CH 0, PG 19
 
+    in_port = mido.open_input('USB MIDI Interface')
+    # out_port = mido.open_output('loopMIDI Port')
+
     last_mode_message = datetime.utcnow() - timedelta(seconds=1)
     MODE_MESSAGE = mido.Message("control_change", channel=0, control=11, value=0, time=0)
-    with mido.open_output('PY OUT', virtual=True, client_name='PY OUT') as outport:
+    with mido.open_output('loopMIDI Port') as outport:
         with in_port as port:
             for message in port:
                 print("receiving message", message)
-                print("sending message to PY OUT", message)
-                outport.send(message)
+                if message.program in STOP_BUTTONS:
+                    out_msg = mido.Message("control_change", channel=0, control=5, value=ON, time=0)
+                elif message.program in REPEAT_BUTTONS:
+                    out_msg = mido.Message("control_change", channel=0, control=6, value=ON, time=0)
+                elif message.program in RESUME_BUTTONS:
+                    out_msg = mido.Message("control_change", channel=0, control=7, value=ON, time=0)
+                else:
+                    out_msg = mido.Message("control_change", channel=0, control=message.program, value=ON, time=0)
+
+                print("sending message to loopMIDI", out_msg)
+                outport.send(out_msg
+                             )
